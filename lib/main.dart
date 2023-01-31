450d61226a9dbb2334d446dc87bedfab3923b5da
Omar Toutounji <omartoutounji@Omars-MacBook-Air.local>
2023-01-31 11:25:37 -0500

    added playback support

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,81 @@
 import 'package:flutter/material.dart';
-import 'package:audioplayers/audioplayers.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:audio_service/audio_service.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'dart:math';
 
-void main() {
+late AudioHandler _audioHandler;
+Future<void> main() async {
+  _audioHandler = await AudioService.init(
+    builder: () => AudioPlayerHandler(),
+    config: const AudioServiceConfig(
+      androidNotificationChannelId: 'com.app.nano.motivation',
+      androidNotificationChannelName: 'Motive',
+      androidNotificationOngoing: true,
+    ),
+  );
   runApp(const MyApp());
+}
+
+class AudioPlayerHandler extends BaseAudioHandler {
+  final _player = AudioPlayer();
+
+  @override
+  AudioPlayerHandler() {
+    int number = next(1, 19);
+
+    // Broadcast that we're loading, and what controls are available.
+    playbackState.add(playbackState.value.copyWith(
+      controls: [MediaControl.play],
+      processingState: AudioProcessingState.loading,
+    ));
+    // Connect to the URL
+    _player
+        .setAudioSource(AudioSource.uri(
+            Uri.parse("asset:///assets/audio/$number.mp3"),
+            tag: MediaItem(
+                id: '$number',
+                title: "Motive",
+                album: "Motive",
+                artUri: Uri.parse("asset:///assets/icon/motive.png"))))
+        .then((_) {
+      // Broadcast that we've finished loading
+      playbackState.add(playbackState.value.copyWith(
+        processingState: AudioProcessingState.ready,
+      ));
+    });
+  }
+
+  int next(int min, int max) => min + Random().nextInt(max - min);
+
+  @override
+  Future<void> play() async {
+    playbackState.add(playbackState.value.copyWith(
+      playing: true,
+      controls: [MediaControl.pause],
+    ));
+    await _player.play();
+  }
+
+  @override
+  Future<void> stop() async {
+    // Release any audio decoders back to the system
+    await _player.stop();
+
+    // Set the audio_service state to `idle` to deactivate the notification.
+    playbackState.add(playbackState.value.copyWith(
+      processingState: AudioProcessingState.idle,
+    ));
+  }
+
+  @override
+  Future<void> pause() async {
+    playbackState.add(playbackState.value.copyWith(
+      playing: false,
+      controls: [MediaControl.play],
+    ));
+    await _player.pause();
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -20,58 +92,8 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MotivePlayer extends StatefulWidget {
+class MotivePlayer extends StatelessWidget {
   const MotivePlayer({super.key});
-
-  @override
-  State<MotivePlayer> createState() => _MotivePlayerState();
-}
-
-class _MotivePlayerState extends State<MotivePlayer> {
-  bool _play = false;
-  AudioPlayer player = AudioPlayer();
-
-  @override
-  void initState() {
-    super.initState();
-    int number = next(1, 19);
-    _load("audio/$number.mp3");
-  }
-
-  @override
-  void dispose() {
-    _remove();
-    super.dispose();
-  }
-
-  _remove() async {
-    await player.dispose();
-  }
-
-  _load(path) async {
-    await player.setSource(AssetSource(path));
-  }
-
-  _pause() async {
-    await player.pause();
-  }
-
-  _resume() async {
-    await player.resume();
-  }
-
-  int next(int min, int max) => min + Random().nextInt(max - min);
-
-  void switchPlay() {
-    setState(() {
-      _play = !_play;
-    });
-    if (_play) {
-      _resume();
-    } else {
-      _pause();
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -83,12 +105,15 @@ class _MotivePlayerState extends State<MotivePlayer> {
           children: [
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
+              children: [
                 Text(
                   "Tap to start motive",
-                  style: TextStyle(color: Colors.grey, fontSize: 30),
+                  style: GoogleFonts.raleway(
+                      fontWeight: FontWeight.w100,
+                      fontSize: 30,
+                      color: Colors.grey),
                 ),
-                Image(
+                const Image(
                   image: AssetImage('assets/icon/motive.png'),
                   width: 70,
                   height: 70,
@@ -98,19 +123,31 @@ class _MotivePlayerState extends State<MotivePlayer> {
             const SizedBox(
               height: 50.0,
             ),
-            RawMaterialButton(
-              onPressed: () {
-                switchPlay();
+            StreamBuilder<PlaybackState>(
+              stream: _audioHandler.playbackState,
+              builder: (context, snapshot) {
+                final playing = snapshot.data?.playing ?? false;
+                final processingState =
+                    snapshot.data?.processingState ?? AudioProcessingState.idle;
+                return (RawMaterialButton(
+                  onPressed: () {
+                    if (playing) {
+                      _audioHandler.pause();
+                    } else {
+                      _audioHandler.play();
+                    }
+                  },
+                  elevation: 2.0,
+                  fillColor: Colors.white,
+                  padding: const EdgeInsets.all(15.0),
+                  shape: const CircleBorder(),
+                  child: Icon(
+                    playing ? Icons.pause : Icons.play_arrow,
+                    size: 100.0,
+                  ),
+                ));
               },
-              elevation: 2.0,
-              fillColor: Colors.white,
-              padding: const EdgeInsets.all(15.0),
-              shape: const CircleBorder(),
-              child: Icon(
-                _play ? Icons.pause : Icons.play_arrow,
-                size: 100.0,
-              ),
-            ),
+            )
           ],
         ),
       ),
